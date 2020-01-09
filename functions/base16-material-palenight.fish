7d27ec7ffc2128bf137734416b3a08ab5d0419ea
Kristopher Ruzic <krruzic@gmail.com>
2020-01-08 21:19:18 -0700

    shared code 2

--- functions/base16-material-palenight.fish
+++ functions/base16-material-palenight.fish
@@ -3,119 +3,32 @@
 # Material Palenight scheme by Nate Peterson
 
 function base16-material-palenight -d "Material Palenight"
-  set color00 '#292D3E' # Base 00 - Black
-  set color01 '#F07178' # Base 08 - Red
-  set color02 '#C3E88D' # Base 0B - Green
-  set color03 '#FFCB6B' # Base 0A - Yellow
-  set color04 '#82AAFF' # Base 0D - Blue
-  set color05 '#C792EA' # Base 0E - Magenta
-  set color06 '#89DDFF' # Base 0C - Cyan
-  set color07 '#959DCB' # Base 05 - White
-  set color08 '#676E95' # Base 03 - Bright Black
-  set color09 $color01 # Base 08 - Bright Red
-  set color10 $color02 # Base 0B - Bright Green
-  set color11 $color03 # Base 0A - Bright Yellow
-  set color12 $color04 # Base 0D - Bright Blue
-  set color13 $color05 # Base 0E - Bright Magenta
-  set color14 $color06 # Base 0C - Bright Cyan
-  set color15 '#FFFFFF' # Base 07 - Bright White
-  set color16 '#F78C6C' # Base 09
-  set color17 '#FF5370' # Base 0F
-  set color18 '#444267' # Base 01
-  set color19 '#32374D' # Base 02
-  set color20 '#8796B0' # Base 04
-  set color21 '#959DCB' # Base 06
-  set colorfg $color07 # Base 05 - White
-  set colorbg $color00 # Base 00 - Black
-  function hex_to_rgb;
-    echo $argv[1] | sed -rE 's/\#([0-9A-H]{2})([0-9A-H]{2})([0-9A-H]{2})/\1\/\2\/\3/gI'
-  end;
-
-  function put_template; printf '\033Ptmux;\033\033]%d;rgb:%s\007\033\\' $argv; end;
-  function put_template_var; printf '\033Ptmux;\033\033]%d;rgb:%s\007\033\\' $argv; end;
-  function put_template_custom; printf '\033]%s%s\033\\' $argv; end;
-
-  # 16 color space
-  put_template 0  (hex_to_rgb $color00)
-  put_template 1  (hex_to_rgb $color01)
-  put_template 2  (hex_to_rgb $color02)
-  put_template 3  (hex_to_rgb $color03)
-  put_template 4  (hex_to_rgb $color04)
-  put_template 5  (hex_to_rgb $color05)
-  put_template 6  (hex_to_rgb $color06)
-  put_template 7  (hex_to_rgb $color07)
-  put_template 8  (hex_to_rgb $color08)
-  put_template 9  (hex_to_rgb $color09)
-  put_template 10 (hex_to_rgb $color10)
-  put_template 11 (hex_to_rgb $color11)
-  put_template 12 (hex_to_rgb $color12)
-  put_template 13 (hex_to_rgb $color13)
-  put_template 14 (hex_to_rgb $color14)
-  put_template 15 (hex_to_rgb $color15)
-
-  # 256 color space
-  put_template 16 (hex_to_rgb $color16)
-  put_template 17 (hex_to_rgb $color17)
-  put_template 18 (hex_to_rgb $color18)
-  put_template 19 (hex_to_rgb $color19)
-  put_template 20 (hex_to_rgb $color20)
-  put_template 21 (hex_to_rgb $color21)
-
-  # foreground / background / cursor color
-  if test -n "$ITERM_SESSION_ID"
-    # iTerm2 proprietary escape codes
-    put_template_custom Pg 959DCB # foreground
-    put_template_custom Ph 292D3E # background
-    put_template_custom Pi 959DCB # bold color
-    put_template_custom Pj 32374D # selection color
-    put_template_custom Pk 959DCB # selected text color
-    put_template_custom Pl 959DCB # cursor
-    put_template_custom Pm 292D3E # cursor text
-  else
-    put_template_var 10 $colorfg
-    if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]
-      put_template_var 11 $colorbg
-      if string match 'rxvt*' $TERM # [ "${TERM%%-*}" = "rxvt" ]
-        put_template_var 708 $colorbg # internal border (rxvt)
-      end
-    end
-    put_template_custom 12 ";7" # cursor (reverse video)
-  end
-
-  # set syntax highlighting colors
-  set -U fish_color_autosuggestion 32374D
-  set -U fish_color_cancel -r
-  set -U fish_color_command green #white
-  set -U fish_color_comment 32374D
-  set -U fish_color_cwd green
-  set -U fish_color_cwd_root red
-  set -U fish_color_end brblack #blue
-  set -U fish_color_error red
-  set -U fish_color_escape yellow #green
-  set -U fish_color_history_current --bold
-  set -U fish_color_host normal
-  set -U fish_color_match --background=brblue
-  set -U fish_color_normal normal
-  set -U fish_color_operator blue #green
-  set -U fish_color_param 8796B0
-  set -U fish_color_quote yellow #brblack
-  set -U fish_color_redirection cyan
-  set -U fish_color_search_match bryellow --background=32374D
-  set -U fish_color_selection white --bold --background=32374D
-  set -U fish_color_status red
-  set -U fish_color_user brgreen
-  set -U fish_color_valid_path --underline
-  set -U fish_pager_color_completion normal
-  set -U fish_pager_color_description yellow --dim
-  set -U fish_pager_color_prefix white --bold #--underline
-  set -U fish_pager_color_progress brwhite --background=cyan
-  # FZF theme
-  # FZF theme
-  set -u FZF_DEFAULT_OPTS "--color=bg+:$color01,bg:$color00,spinner:$color12,hl:$color13 --color=fg:$color04,header:$color13,info:$color10,pointer:$color12 --color=marker:$color12,fg+:$color06,prompt:$color10,hl+:$color13"
-
-  # remember current theme
-  set -U base16_theme material-palenight
-
-  # clean up
-  functions -e put_template put_template_var put_template_custom
+  set -gx color00 '#292D3E' # Base 00 - Black
+  set -gx color01 '#F07178' # Base 08 - Red
+  set -gx color02 '#C3E88D' # Base 0B - Green
+  set -gx color03 '#FFCB6B' # Base 0A - Yellow
+  set -gx color04 '#82AAFF' # Base 0D - Blue
+  set -gx color05 '#C792EA' # Base 0E - Magenta
+  set -gx color06 '#89DDFF' # Base 0C - Cyan
+  set -gx color07 '#959DCB' # Base 05 - White
+  set -gx color08 '#676E95' # Base 03 - Bright Black
+  set -gx color09 $color01 # Base 08 - Bright Red
+  set -gx color10 $color02 # Base 0B - Bright Green
+  set -gx color11 $color03 # Base 0A - Bright Yellow
+  set -gx color12 $color04 # Base 0D - Bright Blue
+  set -gx color13 $color05 # Base 0E - Bright Magenta
+  set -gx color14 $color06 # Base 0C - Bright Cyan
+  set -gx color15 '#FFFFFF' # Base 07 - Bright White
+  set -gx color16 '#F78C6C' # Base 09
+  set -gx color17 '#FF5370' # Base 0F
+  set -gx color18 '#444267' # Base 01
+  set -gx color19 '#32374D' # Base 02
+  set -gx color20 '#8796B0' # Base 04
+  set -gx color21 '#959DCB' # Base 06
+  set -gx colorfg $color07 # Base 05 - White
+  set -gx colorbg $color00 # Base 00 - Black
+  source base16.fish
+  set_colors
+  set_fzf
+  functions -e put_template put_template_var put_template_custom set_fzf set_colors hex_without_hash hex_to_rgb
 end
--- functions/base16-unikitty-dark.fish
+++ functions/base16-unikitty-dark.fish
@@ -3,119 +3,32 @@
 # Unikitty Dark scheme by Josh W Lewis (@joshwlewis)
 
 function base16-unikitty-dark -d "Unikitty Dark"
-  set color00 '#2e2a31' # Base 00 - Black
-  set color01 '#d8137f' # Base 08 - Red
-  set color02 '#17ad98' # Base 0B - Green
-  set color03 '#dc8a0e' # Base 0A - Yellow
-  set color04 '#796af5' # Base 0D - Blue
-  set color05 '#bb60ea' # Base 0E - Magenta
-  set color06 '#149bda' # Base 0C - Cyan
-  set color07 '#bcbabe' # Base 05 - White
-  set color08 '#838085' # Base 03 - Bright Black
-  set color09 $color01 # Base 08 - Bright Red
-  set color10 $color02 # Base 0B - Bright Green
-  set color11 $color03 # Base 0A - Bright Yellow
-  set color12 $color04 # Base 0D - Bright Blue
-  set color13 $color05 # Base 0E - Bright Magenta
-  set color14 $color06 # Base 0C - Bright Cyan
-  set color15 '#f5f4f7' # Base 07 - Bright White
-  set color16 '#d65407' # Base 09
-  set color17 '#c720ca' # Base 0F
-  set color18 '#4a464d' # Base 01
-  set color19 '#666369' # Base 02
-  set color20 '#9f9da2' # Base 04
-  set color21 '#d8d7da' # Base 06
-  set colorfg $color07 # Base 05 - White
-  set colorbg $color00 # Base 00 - Black
-  function hex_to_rgb;
-    echo $argv[1] | sed -rE 's/\#([0-9A-H]{2})([0-9A-H]{2})([0-9A-H]{2})/\1\/\2\/\3/gI'
-  end;
-
-  function put_template; printf '\033Ptmux;\033\033]%d;rgb:%s\007\033\\' $argv; end;
-  function put_template_var; printf '\033Ptmux;\033\033]%d;rgb:%s\007\033\\' $argv; end;
-  function put_template_custom; printf '\033]%s%s\033\\' $argv; end;
-
-  # 16 color space
-  put_template 0  (hex_to_rgb $color00)
-  put_template 1  (hex_to_rgb $color01)
-  put_template 2  (hex_to_rgb $color02)
-  put_template 3  (hex_to_rgb $color03)
-  put_template 4  (hex_to_rgb $color04)
-  put_template 5  (hex_to_rgb $color05)
-  put_template 6  (hex_to_rgb $color06)
-  put_template 7  (hex_to_rgb $color07)
-  put_template 8  (hex_to_rgb $color08)
-  put_template 9  (hex_to_rgb $color09)
-  put_template 10 (hex_to_rgb $color10)
-  put_template 11 (hex_to_rgb $color11)
-  put_template 12 (hex_to_rgb $color12)
-  put_template 13 (hex_to_rgb $color13)
-  put_template 14 (hex_to_rgb $color14)
-  put_template 15 (hex_to_rgb $color15)
-
-  # 256 color space
-  put_template 16 (hex_to_rgb $color16)
-  put_template 17 (hex_to_rgb $color17)
-  put_template 18 (hex_to_rgb $color18)
-  put_template 19 (hex_to_rgb $color19)
-  put_template 20 (hex_to_rgb $color20)
-  put_template 21 (hex_to_rgb $color21)
-
-  # foreground / background / cursor color
-  if test -n "$ITERM_SESSION_ID"
-    # iTerm2 proprietary escape codes
-    put_template_custom Pg bcbabe # foreground
-    put_template_custom Ph 2e2a31 # background
-    put_template_custom Pi bcbabe # bold color
-    put_template_custom Pj 666369 # selection color
-    put_template_custom Pk bcbabe # selected text color
-    put_template_custom Pl bcbabe # cursor
-    put_template_custom Pm 2e2a31 # cursor text
-  else
-    put_template_var 10 $colorfg
-    if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]
-      put_template_var 11 $colorbg
-      if string match 'rxvt*' $TERM # [ "${TERM%%-*}" = "rxvt" ]
-        put_template_var 708 $colorbg # internal border (rxvt)
-      end
-    end
-    put_template_custom 12 ";7" # cursor (reverse video)
-  end
-
-  # set syntax highlighting colors
-  set -U fish_color_autosuggestion 666369
-  set -U fish_color_cancel -r
-  set -U fish_color_command green #white
-  set -U fish_color_comment 666369
-  set -U fish_color_cwd green
-  set -U fish_color_cwd_root red
-  set -U fish_color_end brblack #blue
-  set -U fish_color_error red
-  set -U fish_color_escape yellow #green
-  set -U fish_color_history_current --bold
-  set -U fish_color_host normal
-  set -U fish_color_match --background=brblue
-  set -U fish_color_normal normal
-  set -U fish_color_operator blue #green
-  set -U fish_color_param 9f9da2
-  set -U fish_color_quote yellow #brblack
-  set -U fish_color_redirection cyan
-  set -U fish_color_search_match bryellow --background=666369
-  set -U fish_color_selection white --bold --background=666369
-  set -U fish_color_status red
-  set -U fish_color_user brgreen
-  set -U fish_color_valid_path --underline
-  set -U fish_pager_color_completion normal
-  set -U fish_pager_color_description yellow --dim
-  set -U fish_pager_color_prefix white --bold #--underline
-  set -U fish_pager_color_progress brwhite --background=cyan
-  # FZF theme
-  # FZF theme
-  set -u FZF_DEFAULT_OPTS "--color=bg+:$color01,bg:$color00,spinner:$color12,hl:$color13 --color=fg:$color04,header:$color13,info:$color10,pointer:$color12 --color=marker:$color12,fg+:$color06,prompt:$color10,hl+:$color13"
-
-  # remember current theme
-  set -U base16_theme unikitty-dark
-
-  # clean up
-  functions -e put_template put_template_var put_template_custom
+  set -gx color00 '#2e2a31' # Base 00 - Black
+  set -gx color01 '#d8137f' # Base 08 - Red
+  set -gx color02 '#17ad98' # Base 0B - Green
+  set -gx color03 '#dc8a0e' # Base 0A - Yellow
+  set -gx color04 '#796af5' # Base 0D - Blue
+  set -gx color05 '#bb60ea' # Base 0E - Magenta
+  set -gx color06 '#149bda' # Base 0C - Cyan
+  set -gx color07 '#bcbabe' # Base 05 - White
+  set -gx color08 '#838085' # Base 03 - Bright Black
+  set -gx color09 $color01 # Base 08 - Bright Red
+  set -gx color10 $color02 # Base 0B - Bright Green
+  set -gx color11 $color03 # Base 0A - Bright Yellow
+  set -gx color12 $color04 # Base 0D - Bright Blue
+  set -gx color13 $color05 # Base 0E - Bright Magenta
+  set -gx color14 $color06 # Base 0C - Bright Cyan
+  set -gx color15 '#f5f4f7' # Base 07 - Bright White
+  set -gx color16 '#d65407' # Base 09
+  set -gx color17 '#c720ca' # Base 0F
+  set -gx color18 '#4a464d' # Base 01
+  set -gx color19 '#666369' # Base 02
+  set -gx color20 '#9f9da2' # Base 04
+  set -gx color21 '#d8d7da' # Base 06
+  set -gx colorfg $color07 # Base 05 - White
+  set -gx colorbg $color00 # Base 00 - Black
+  source base16.fish
+  set_colors
+  set_fzf
+  functions -e put_template put_template_var put_template_custom set_fzf set_colors hex_without_hash hex_to_rgb
 end
